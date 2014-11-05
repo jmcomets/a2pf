90e116f0629ac62622ce0bf12564b8ec1e038251
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-11-05 13:28:25 +0100

    Add file handling

--- intro.hs
+++ intro.hs
@@ -16,26 +16,38 @@ countRuns [] = []
 countRuns (w:ws) = (1 + length us, w) : countRuns vs
                      where (us, vs) = span (== w) ws
 
+type Token = String
+
 -- Split a string into tokens, which are separated by space or punctuation.
+tokens :: String -> [Token]
 tokens = split (\ c -> isSpace c || isPunctuation c)
 
 -- Count the tokens in a string, returning the list of pairs (number of
 -- occurences, token) for each token. The tokens are listed in reverse order of
 -- occurence.
-countTokens :: String -> [(Int, String)]
+countTokens :: String -> [(Int, Token)]
 countTokens = countRuns . sort . tokens . map toLower
+
+-- Show a single token count
+showTokenCount :: (Int, Token) -> String
+showTokenCount (n, t) = t ++ "," ++ show n
 
 -- Take the n most occurences of tokens in String s (see countTokens).
 commonWords 0 s = countTokens s
 commonWords n s = take n $ reverse $ commonWords 0 s
 
+-- Show all entries of a commonWords return
+showCommonWords :: Int -> String -> String
+showCommonWords n = concat . map showTokenCount . take n . countTokens
+
 -- Main program
 main =
-  do putStr "Input file? "
+  do putStrLn "Input file? "
      inFile <- getLine
-     putStr "Output file? "
-     outfile <- getLine
-     putStr "Word limit? (0: infinity) "
+     fileContents <- readFile inFile
+     putStrLn "Output file? "
+     outFile <- getLine
+     putStrLn "Word limit? (0: infinity) "
      n <- getLine
-     putStrLn "Ok"
-     --putStrLn (intercalate "\n" (map (\ (x, y) -> (show x) ++ ": " ++ y) (commonWords (read n) text)))
+     writeFile outFile $ showCommonWords (read n) fileContents
+     putStrLn $ "Output written to " ++ outFile
