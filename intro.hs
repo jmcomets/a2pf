c43b05bb7ac1d9a0fbc070a2faeaabb1d4f6bb51
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-11-05 14:34:21 +0100

    Some option handling

--- intro.hs
+++ intro.hs
@@ -1,13 +1,10 @@
 module Main where
 
-import Control.Monad
 import Data.Char
 import Data.List
 import System.Console.GetOpt
-import System.Environment
-import System.Exit
-import System.IO
-import Text.Printf
+import System.Environment ( getArgs )
+import Data.Maybe ( fromMaybe )
 
 -- Split a string by splitting it along a predicate
 split :: (Char -> Bool) -> String -> [String]
@@ -16,21 +13,20 @@ split f s = case dropWhile f s of
                     s' -> w : split f s''
                               where (w, s'') = break f s'
 
--- Count the number of occurences of the first element of the list and return
--- it as a list of pairs.
+-- Count the number of occurences of the first element of the list and return it as a list of pairs.
 countRuns [] = []
 countRuns (w:ws) = (1 + length us, w) : countRuns vs
                      where (us, vs) = span (== w) ws
 
+-- Token abstraction
 type Token = String
 
 -- Split a string into tokens, which are separated by space or punctuation.
 tokens :: String -> [Token]
 tokens = split (\ c -> isSpace c || isPunctuation c)
 
--- Count the tokens in a string, returning the list of pairs (number of
--- occurences, token) for each token. The tokens are listed in reverse order of
--- occurence.
+-- Count the tokens in a string, returning the list of pairs (number of occurences, token) for each
+-- token. The tokens are listed in reverse order of occurence.
 countTokens :: String -> [(Int, Token)]
 countTokens = countRuns . sort . tokens . map toLower
 
@@ -47,28 +43,22 @@ showCommonWords :: Int -> String -> String
 showCommonWords n = concat . map showTokenCount . take n . countTokens
 
 -- Possible command line flags
-data Flag = Output String | Input String | Help
+data Flag = Output String | Input String | Help deriving Show
 options :: [OptDescr Flag]
 options =
-    [ Option ['o'] ["output"] (OptArg Output "FILE") "Output file"
-    , Option ['i'] ["input"] (ReqArg Input "FILE") "Input file"
-    , Option ['h'] ["help"] (NoArg Help) "Show help"
+    [ Option ['o'] ["output"] (OptArg (Output . fromMaybe "stdout") "FILE") "Output file"
+    , Option ['i'] ["input" ] (ReqArg Input "FILE"                        ) "Input file"
+    , Option ['h'] ["help"  ] (NoArg  Help                                ) "Show help"
     ]
 
+-- Parse command line options
 cliOpts :: [String] -> IO ([Flag], [String])
 cliOpts argv = case getOpt Permute options argv of
                     (o, n, []  ) -> return (o, n)
                     (_, _, errs) -> ioError (userError (concat errs ++ usageInfo header options))
                                     where header = "Usage: intro [OPTION...]"
 
--- Main program (old)
-main =
-  do putStrLn "Input file? "
-     inFile <- getLine
-     fileContents <- readFile inFile
-     putStrLn "Output file? "
-     outFile <- getLine
-     putStrLn "Word limit? (0: infinity) "
-     n <- getLine
-     writeFile outFile $ showCommonWords (read n) fileContents
-     putStrLn $ "Output written to " ++ outFile
+-- Main program
+main = do args <- getArgs
+          (opts, n) <- cliOpts args
+          print opts
